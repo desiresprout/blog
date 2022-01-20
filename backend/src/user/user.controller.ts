ecec469b02eea62e4c1c4c90179cf3d50683c89e
mara <desiresprout@gmail.com>
2022-01-20 23:19:11 +0900

    feat : set redis session and set cookie during login and logout api

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Res, Controller, Post, Get, ForbiddenException, UseGuards, Response } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Body, Req, Res, Controller, Post, Get, ForbiddenException, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './user.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { CacheService } from '../cache/cache.service';
+import { SessionService } from '../cache/session.service';
 import { LoginAuthGuard } from '../auth/auth.guards';
 import { AuthService } from '../auth/auth.service';
+import { nanoid } from 'nanoid';
 
 @ApiTags('유저')
 @Controller()
@@ -14,7 +16,7 @@ export class UserController {
     private readonly userService: UsersService,
     private readonly authService: AuthService,
     private readonly prisma: PrismaService,
-    private readonly cacheService: CacheService
+    private readonly sessionService: SessionService
   ) {}
 
   @ApiOperation({ summary: '회원가입' })
@@ -40,27 +42,43 @@ export class UserController {
 
   @ApiOperation({ summary: '로그인' })
   @Post('login')
-  @UseGuards(LoginAuthGuard)
-  async login(@Body('email') email: string, @Body('password') password: string, @Response() res) {
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({ passthrough: true }) res: Response
+  ) {
     const user = await this.authService.validateUser(email, password);
 
     if (!user) {
       throw new ForbiddenException();
     }
 
-    const session = await this.cacheService.setSession(email);
-    res.setCookie('sid', session);
+    const deviceID = nanoid();
 
-    return {};
+    await this.sessionService.setSession(email, deviceID);
+    res.cookie('sid', email, {
+      signed: true,
+    });
+    res.cookie('deviceID', deviceID);
+
+    return 'ok';
   }
 
-  @ApiOperation({ summary: '로그아웃' })
   @UseGuards(LoginAuthGuard)
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
-  async logout(@Response() res) {
-    await this.cacheService.clearSession('aaa');
-    res.clearCookie('sid');
+  async logout(@Res({ passthrough: true }) res: Response, @Req() req) {
+    const deviceID = req?.cookies.deviceID;
+    const sID = req?.signedCookies.sid;
 
-    return res.send('ok');
+    if (!deviceID || !sID) {
+      throw new ForbiddenException();
+    }
+
+    await this.sessionService.clearSession(sID, deviceID);
+    res.clearCookie('sid');
+    res.clearCookie('deviceID');
+
+    return 'ok';
   }
 }
