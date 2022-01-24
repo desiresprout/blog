7e91b6b10e823abd80791e50eb58255050c753fc
mara <desiresprout@gmail.com>
2022-01-24 23:45:52 +0900

    chore : add document file with global css

---
+++ frontend-ts/pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { css, Global } from '@emotion/react';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+  render() {
+    return (
+      <Html>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/earlyaccess/notosanskr.css"
+          rel="stylesheet"
+        />
+        <Head />
+        <Global
+          styles={css`
+            * {
+              box-sizing: border-box;
+              padding: 0;
+              margin: 0;
+            }
+            body {
+              background-color: rgb(248, 249, 250);
+            }
+          `}
+        />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
