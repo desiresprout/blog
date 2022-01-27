31abb323dfd728a80dc029294e825dec41aa5470
desiresprout <desiresprout@gmail.com>
2022-01-27 18:13:24 +0900

    fix : api nullable fix and add return type

--- frontend-ts/pages/api/posts.ts
+++ frontend-ts/pages/api/posts.ts
@@ -1,11 +1,10 @@
 import ky from 'ky';
+import IPost from '../../types/interface/post';
 
-export function loadPosts(cursor: string, userID?: string) {
-  return ky
-    .get(`http://localhost:4000/posts?cursor=${cursor}&userID=${userID || 0}`)
-    .json();
+export function loadPosts(cursor?: string, userID?: string): Promise<IPost[]> {
+  return ky.get(`http://localhost:4000/posts?cursor=${cursor || 0}&userID=${userID || 0}`).json();
 }
 
-export function loadPost(postID: string) {
+export function loadPost(postID: string): Promise<IPost> {
   return ky.get(`http://localhost:4000/post?postID=${postID}`).json();
 }
