44898677a71d1dc36cea14568b6b6e034c445d47
mara <desiresprout@gmail.com>
2022-02-01 21:51:17 +0900

    chore : unuse css delete

--- frontend-ts/pages/recent.tsx
+++ frontend-ts/pages/recent.tsx
@@ -40,7 +40,6 @@ const Recent = () => {
       <div
         css={css`
           display: inline-block;
-          border: 1px solid red;
           background: #ffffff;
           ${media.xlarge} {
             width: 38.7rem;
